b7ea54986ad7c09bf9354a3e40f649cee39b2d10
Przemo1211 <121610311+Przemo1211@users.noreply.github.com>
2023-01-18 21:01:22 +0100

    Zaktualizowano

---
+++ ksiezyc.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main() {
+  cout << "Księżyc to jedyny naturalny satelita Ziemi."<<endl;
+	cout << "Jest piątym co do wielkości księżycem w Układzie Słonecznym.";
+ return 0;
+}
